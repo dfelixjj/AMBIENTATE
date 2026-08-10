23f67e7dfdaaa79b854abad902c906399daff92c
infoandalmet-gif <info.andalmet@gmail.com>
2026-08-10 13:15:41 +0200

    feat: conversión de la app en PWA instalable en móviles (manifest.json + Service Worker sw.js)

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'ambientate-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './presentacion.html',
+  './logo.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
