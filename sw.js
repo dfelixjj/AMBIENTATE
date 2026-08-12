cbd115197d031afb7dafd3c899d9990953a3cc2f
infoandalmet-gif <info.andalmet@gmail.com>
2026-08-12 10:53:49 +0200

    Fix: proteccion contra pantalla en blanco, eliminacion de facturacion/banco y mejora de autocuracion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ambientate-v9';
+const CACHE_NAME = 'ambientate-v10';
 const ASSETS = [
   './',
   './index.html',
@@ -9,13 +9,18 @@ const ASSETS = [
 
 self.addEventListener('install', event => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS).catch(err => console.warn('Cache pre-fill warning:', err));
+    })
+  );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys.map(k => caches.delete(k))
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
       );
     })
   );
